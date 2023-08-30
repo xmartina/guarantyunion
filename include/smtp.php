c7a4e94d29600ddb950f8a77568f42286d659d69
unknown <martinsprince126@gmail.com>
2023-08-29 17:21:56 -1000

    fixed: site template

--- include/smtp.php
+++ include/smtp.php
@@ -10,18 +10,18 @@ class message{
         $mail = new PHPMailer();
         //SMTP Settings
         $mail->isSMTP();
-        $mail->Host = "mail.finerytrust.com"; // Change Email Host
+        $mail->Host = "mail.guarantyunion.com"; // Change Email Host
         $mail->SMTPAuth = true;
-        $mail->Username = "info@finerytrust.com"; // Change Email Address
-        $mail->Password = '+Finerytrust123'; // Change Email Password
+        $mail->Username = "contact@guarantyunion.com"; // Change Email Address
+        $mail->Password = '+Guarantyunion123'; // Change Email Password
         $mail->Port = 465; //587
         $mail->SMTPSecure = "ssl"; //tls
 
         //Email Settings
         $mail->isHTML(true);
-        $mail->setFrom('info@finerytrust.com','Finery Trust PLC'); // Change
+        $mail->setFrom('contact@guarantyunion.com','Guaranty Union PLC'); // Change
         $mail->addAddress($email);
-        $mail->AddReplyTo("info@finerytrust.com", "Finery Trust PLC"); // Change
+        $mail->AddReplyTo("contact@guarantyunion.com", "Guaranty Union PLC"); // Change
         $mail->Subject = $subject;
         $mail->MsgHTML($message);
         $mail->Send();
